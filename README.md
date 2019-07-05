a991fb7ba241bf12bf728e2661a136e1072b92e7
Will Brown <willbrown@Wills-MacBook-Air.local>
2019-07-05 15:55:20 -0700

    added edge case handling for decimals, + and -, etc

--- README.md
+++ README.md
@@ -66,3 +66,13 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+###
+
++ = will add the current value on the screen to itself
+
+continually pressing equals does nothing
+
+TODO:
+
+Implement shrinking view pane as numbers get too big
--- src/App.js
+++ src/App.js
@@ -8,79 +8,189 @@ class App extends Component {
         super();
 
         this.state = {
-            displayedValue: "0",
-            storedValue: ""
+            displayedValue: '',
+            entries: [],
+            newEntry: false,
+            firstEntryMade: false, //here just to handle the edge case of an initial negative number
         }
     }
 
     onClick = button => {
 
-        if(button === "="){
-            this.calculate()
+        if(button === "=") {
+
+          if (this.isEntriesFull()) {
+            this.calculate(button, this.state.displayedValue)
+          }
+
+          else {
+            return //do nothing this is not a valid operation
+          }
+
+        }
+
+        else if (button === '.' && this.state.newEntry === false){
+
+          this.setState({
+            displayedValue: '0.',
+            newEntry: true,
+            firstEntryMade: true,
+
+          })
+
         }
 
         else if(button === "C"){
             this.reset()
         }
+
         else if(button === "CE"){
             this.backspace()
         }
+        // handle + and - and edge cases
+        else if (button === "+" || button === "-") {
 
-        else {
-          if (this.state.displayedValue === "0") {
+          if (this.state.firstEntryMade === true &&  // user is repeatedly hitting the + or - button and an inital entry has been made
+            this.state.newEntry === false &&
+            this.isEntriesFull()) {
+
+            this.setOperand(button)
+            console.log(this.state)
+
+            return
+          }
+          let temp = []
+          console.log('store:',this.state.entries, 'display:',this.state.displayedValue)
+          if (this.state.firstEntryMade === false ) { //allow negative numbers as first entry if not previous entries
             this.setState({
-                displayedValue: button
+              displayedValue: button,
+              newEntry: true,
+              firstEntryMade: true,
             })
-          } else {
+          }
 
+          else if (this.isEntriesEmpty()) {
+            temp.push(this.state.displayedValue)
+            temp.push(button)
             this.setState({
-                displayedValue: this.state.displayedValue + button
+              entries: temp,
+              newEntry: false,
+              firstEntryMade: true,
             })
+          }
 
+          else if (this.isEntriesFull()) {
+              this.calculate(button, this.state.displayedValue)
+          }
+
+          else if (this.containsFirstOperandOnly()) {
+              console.log('first operand')
+              temp = this.state.entries.slice(0)
+              temp.push(button)
+              this.setState({
+                entries: temp,
+                newEntry: false,
+                firstEntryMade: true,
+              })
           }
         }
+
+        // handle all other button presses
+        else {
+          if (this.state.newEntry === false) {  // set viewing pane when no value is displayed
+            this.setState({
+                displayedValue: button,
+                newEntry: true,
+                firstEntryMade: true,
+            })
+          } else {                                 // set normally
+            this.setState({
+                displayedValue: this.state.displayedValue + button,
+                newEntry: true,
+                firstEntryMade: true,
+            })
+          }
+        }
+      // handle decimals
+      // handle really big numbers
     };
 
+    isEntriesFull = () => {
+      return this.state.entries.length === 2
+    }
 
-    calculate = () => {
-        var checkdisplayedValue = ''
-        if(this.state.displayedValue.includes('--')){
-            checkdisplayedValue = this.state.displayedValue.replace('--','+')
+    setOperand = (button) => {
+      let temp = this.state.entries.slice(0,1)
+      temp.push(button)
+      console.log(temp)
+
+      this.setState({
+        entries: temp,
+      })
+    }
+
+    isValidOperation = (operationArray) => {
+      let firstNum = operationArray[0]
+      let operator = operationArray[1]
+      let secondNum = operationArray[2]
+      let regExTestForNum = /^-?[0-9]\d*(\.\d+)?$/
+
+      return regExTestForNum.test(firstNum) &&
+             regExTestForNum.test(secondNum) &&
+             (operator === '+' || operator === '-')
+    }
+
+    containsFirstOperandOnly = () => {
+      return this.state.entries.length === 1
+    }
+
+    isEntriesEmpty = () => {
+      return this.state.entries.length === 0
+    }
+
+    checkDisplayedValue = () => {
+      return this.state.entries[0] === this.state.displayedValue
+    }
+
+    calculate = (button, lastValue) => {
+
+        let newEntries = []
+        let expression = this.state.entries.slice(0)
+        expression.push(lastValue)
+
+        if (this.isValidOperation(expression)) {
+          let result = eval(expression.join(''))
+          newEntries.push(result)
+        } else {
+          console.log('there was a problem with your expression.  Clear inputs and try again')
+          console.log(expression)
+          return
         }
 
-        else {
-            checkdisplayedValue = this.state.displayedValue
+        if (button !== '=') {
+          newEntries.push(button)
         }
 
-        try {
-            this.setState({
-                // eslint-disable-next-line
-                displayedValue: (eval(checkdisplayedValue) || "" ) + ""
-            })
-        } catch (e) {
-            this.setState({
-                displayedValue: "error"
-            })
-
-        }
+        this.setState({
+          entries: newEntries,
+          displayedValue: newEntries[0],
+          newEntry: false,
+        })
     };
 
     reset = () => {
         this.setState({
-            displayedValue: "0"
+            displayedValue: '',
+            entries: [],
+            newEntry: false,
+            firstEntryMade: false,
         })
     };
 
     backspace = () => {
-      if (this.state.displayedValue.slice(0, -1) === "") {
         this.setState({
-            displayedValue: "0"
+            displayedValue: '',
         })
-      } else {
-        this.setState({
-            displayedValue: this.state.displayedValue.slice(0, -1)
-        })
-      }
     };
 
     render() {
