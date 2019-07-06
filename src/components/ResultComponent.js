24c1c5702d46b49f1265d6aaa03089b8efa72c94
Will Brown <willbrown@Wills-MacBook-Air.local>
2019-07-05 22:28:09 -0700

    added shrinking text when result grows large

--- src/components/ResultComponent.js
+++ src/components/ResultComponent.js
@@ -2,11 +2,34 @@ import React, {Component} from 'react';
 
 class ResultComponent extends Component {
 
+    constructor(){
+        super();
 
+        this.state = {
+            resultClass: 'result', //here just to handle the edge case of an initial negative number
+        }
+    }
+
+    componentWillReceiveProps(props){
+      let newResultClass = ''
+      if (props.displayedValue.length < 10) {
+        newResultClass = 'result'
+      } else if (props.displayedValue.length >= 10 && props.displayedValue.length < 20)
+        newResultClass = 'result-small'
+      else {
+        newResultClass = 'result-very-small'
+      }
+      this.setState({
+        resultClass: newResultClass
+      })
+
+    }
     render() {
         let { displayedValue } = this.props;
+        let { resultClass } = this.state;
+
         return (
-            <div className="result">
+            <div className={ resultClass }>
                 <p>{ displayedValue }</p>
             </div>
     )
